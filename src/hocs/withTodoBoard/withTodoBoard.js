59e4a648b336ddb276a92bcfbfeebf2c6bc60dbf
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-28 17:38:02 +0300

    Set adding a todo

--- src/hocs/withTodoBoard/withTodoBoard.js
+++ src/hocs/withTodoBoard/withTodoBoard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const withTodoBoard = (WrappedComponent) => {
   return class extends React.Component {
@@ -13,8 +14,20 @@ const withTodoBoard = (WrappedComponent) => {
       });
     };
 
+    addTodo = () => {
+      const { todos, inputText } = this.state;
+      const newTodo = {
+        text: inputText,
+        completed: false,
+        id: uuidv4(),
+      };
+      this.setState({
+        todos: [...todos, newTodo],
+      });
+    };
+
     render() {
-      return <WrappedComponent setInputText={this.setInputText} />;
+      return <WrappedComponent setInputText={this.setInputText} addTodo={this.addTodo} />;
     }
   };
 };
