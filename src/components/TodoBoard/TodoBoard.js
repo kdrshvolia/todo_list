e9c0caa0fe398018f35c111f27a4178a4ecffa37
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-28 16:53:58 +0300

    Add TodoBoard

---
+++ src/components/TodoBoard/TodoBoard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Input from '../Input/Input'
+import DateInput from '../DateInput/DateInput'
+
+const TodoBoard = () => {
+  return (
+    <div>
+      <Input placeholder={} onChange={} inputText={} />
+      <DateInput onChange={} value={} />
+      <TodoList />
+    </div>
+  )
+};
+
+export default TodoBoard;
