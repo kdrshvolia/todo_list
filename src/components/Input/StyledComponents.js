06df1ed5be805f0eb47e92fd9b8e9cbcccba7a9c
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-29 15:21:35 +0300

    Update input width

--- src/components/Input/StyledComponents.js
+++ src/components/Input/StyledComponents.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const CustomInput = styled.input`
   padding: 10px;
-  width: 200px;
+  width: 350px;
   border-radius: 5px;
   border: 1px solid #d2d2d2;
   margin: 10px 0;
