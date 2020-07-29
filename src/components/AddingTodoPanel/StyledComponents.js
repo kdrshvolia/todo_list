54d13007c7f46c5e1c94a5b410a877ee00fc5ade
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-29 14:23:36 +0300

    Set styles for modal

--- src/components/AddingTodoPanel/StyledComponents.js
+++ src/components/AddingTodoPanel/StyledComponents.js
@@ -5,6 +5,23 @@ const AddingPanel = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
+  background: #fff;
+  padding: 20px;
+  flex-basis: 100%;
 `;
 
-export { AddingPanel };
+const ModalWrapper = styled.div`
+  display: ${(props) => (props.isModalOpened ? 'flex' : 'none')}; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  align-items: center;
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+`;
+
+export { AddingPanel, ModalWrapper };
