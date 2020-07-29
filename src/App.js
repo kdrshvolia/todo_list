97eab818ede7db3c736defb20b69fcd33fb9625a
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-29 17:04:39 +0300

    Add global style

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Button from './components/Button/Button';
 import { withTodoBoard } from './hocs/withTodoBoard/withTodoBoard';
 import TodoBoard from './components/TodoBoard/TodoBoard';
+import { GlobalStyle } from './StyledComponents';
 
 function App() {
   const EnhancedTodoBoard = withTodoBoard(TodoBoard);
   return (
     <div className="App">
+      <GlobalStyle />
       <EnhancedTodoBoard />
     </div>
   );
