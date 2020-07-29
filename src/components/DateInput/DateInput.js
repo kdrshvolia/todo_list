375bca030025e59e535cd03713811ba5c812c023
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-29 13:27:47 +0300

    Style Custom Date Input

--- src/components/DateInput/DateInput.js
+++ src/components/DateInput/DateInput.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CustomDateInput } from './StyledComponents';
 
 const DateInput = ({ value, onChange }) => {
   const handleChange = ({ target }) => onChange(target.value);
 
-  return <input type="date" value={value} onChange={handleChange} />;
+  return <CustomDateInput type="date" value={value} onChange={handleChange} />;
 };
 
 DateInput.propTypes = {
---
+++ src/components/DateInput/StyledComponents.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const CustomDateInput = styled.input.attrs({ type: 'date' })`
+  border: 1px solid #d2d2d2;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+export { CustomDateInput };
