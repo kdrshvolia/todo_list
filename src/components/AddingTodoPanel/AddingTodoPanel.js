bf9868bc37eec1c6c84a3dc6c2d6c42cd8766396
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-29 16:54:17 +0300

    Handle input focus

--- src/components/AddingTodoPanel/AddingTodoPanel.js
+++ src/components/AddingTodoPanel/AddingTodoPanel.js
@@ -4,17 +4,29 @@ import DateInput from '../DateInput/DateInput';
 import { AddingButton, Wrapper } from '../TodoBoard/StyledComponents';
 import { AddingPanel, CloseCross, ModalWrapper } from './StyledComponents';
 
-const AddingTodoPanel = ({ setInputText, setDate, addTodo, isModalOpened, setModalOpen }) => {
-  return (
-    <ModalWrapper isModalOpened={isModalOpened}>
-      <AddingPanel>
-        <CloseCross onClick={setModalOpen}>&times;</CloseCross>
-        <Input onChange={setInputText} placeholder="Enter a task..." />
-        <DateInput onChange={setDate} />
-        <AddingButton onClick={addTodo}>Add</AddingButton>
-      </AddingPanel>
-    </ModalWrapper>
-  );
-};
+class AddingTodoPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
+  focusTextInput = () => {
+    this.inputRef.current.handleFocus();
+  };
+
+  render() {
+    const { setInputText, setDate, addTodo, isModalOpened, setModalOpen, inputText } = this.props;
+    return (
+      <ModalWrapper isModalOpened={isModalOpened}>
+        <AddingPanel>
+          <CloseCross onClick={setModalOpen}>&times;</CloseCross>
+          <Input onChange={setInputText} placeholder="Enter a task..." ref={this.inputRef} />
+          <DateInput onChange={setDate} />
+          <AddingButton onClick={inputText ? addTodo : this.focusTextInput}>Add</AddingButton>
+        </AddingPanel>
+      </ModalWrapper>
+    );
+  }
+}
 
 export default AddingTodoPanel;
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,13 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CustomInput } from './StyledComponents';
 
-const Input = ({ inputText, onChange, placeholder }) => {
-  const handleChange = ({ target }) => onChange(target.value);
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.input = React.createRef();
+  }
 
-  return (
-    <CustomInput type="text" value={inputText} onChange={handleChange} placeholder={placeholder} />
-  );
-};
+  handleChange = ({ target }) => this.props.onChange(target.value);
+
+  handleFocus = () => this.input.current.focus();
+
+  render() {
+    const { inputText, onChange, placeholder } = this.props;
+    return (
+      <CustomInput
+        type="text"
+        value={inputText}
+        onChange={this.handleChange}
+        placeholder={placeholder}
+        ref={this.input}
+        onFocus={this.handleFocus}
+      />
+    );
+  }
+}
 
 Input.propTypes = {
   inputText: PropTypes.string.isRequired,
