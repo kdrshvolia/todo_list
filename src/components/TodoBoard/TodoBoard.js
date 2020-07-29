80c26ae1c90a7c9c1e11199b1edd395366118080
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-29 17:32:57 +0300

    Add missing prop types

--- src/components/TodoBoard/TodoBoard.js
+++ src/components/TodoBoard/TodoBoard.js
@@ -57,4 +57,33 @@ const TodoBoard = ({
   );
 };
 
+TodoBoard.propTypes = {
+  setInputText: PropTypes.func.isRequired,
+  addTodo: PropTypes.func.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+  searchTodos: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  setSortType: PropTypes.func.isRequired,
+  setDate: PropTypes.func.isRequired,
+  setFilterDate: PropTypes.func.isRequired,
+  setModalOpen: PropTypes.func.isRequired,
+  isModalOpened: PropTypes.bool.isRequired,
+  inputText: PropTypes.string.isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  filters: PropTypes.objectOf({
+    text: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
+  sortType: PropTypes.string.isRequired,
+  sortOptions: PropTypes.arrayOf(
+    PropTypes.shape({ type: PropTypes.string.isRequired, value: PropTypes.string.isRequired }),
+  ).isRequired,
+};
+
 export default TodoBoard;
