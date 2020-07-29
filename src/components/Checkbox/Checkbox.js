7c1d3709bc78f8302c937ff655ecfebf769fd1db
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-07-29 13:19:10 +0300

    Style custom checkbox

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CheckboxContainer, HiddenCheckbox, StyledCheckbox, Icon } from './StyledComponents';
 
 const Checkbox = ({ completed, onChange }) => {
   const handleChange = () => onChange();
-  return <input type="checkbox" name="checkbox" checked={completed} onChange={handleChange} />;
+  return (
+    <CheckboxContainer onClick={handleChange}>
+      <HiddenCheckbox type="checkbox" name="checkbox" checked={completed} />
+      <StyledCheckbox checked={completed}>
+        <Icon viewBox="0 0 24 24">
+          <polyline points="20 6 9 17 4 12" />
+        </Icon>
+      </StyledCheckbox>
+    </CheckboxContainer>
+  );
 };
 
 export default Checkbox;
---
+++ src/components/Checkbox/StyledComponents.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+  border: 0;
+  clip: rect(0 0 0 0);
+  clippath: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`;
+
+const Icon = styled.svg`
+  fill: none;
+  stroke: white;
+  stroke-width: 2px;
+`;
+
+const StyledCheckbox = styled.div`
+  display: inline-block;
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+  box-sizing: border-box;
+  background: ${(props) => (props.checked ? '#95C8BD' : '#fff')};
+  border: ${(props) => (props.checked ? 'none' : '1px solid #d2d2d2')};
+  border-radius: 3px;
+  transition: all 150ms;
+
+  ${HiddenCheckbox}:focus + & {
+    box-shadow: 0 0 0 3px pink;
+  }
+
+  ${Icon} {
+    visibility: ${(props) => (props.checked ? 'visible' : 'hidden')};
+  }
+`;
+
+const CheckboxContainer = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+export { HiddenCheckbox, StyledCheckbox, CheckboxContainer, Icon };
