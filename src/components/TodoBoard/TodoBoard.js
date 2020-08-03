1f32cfcfd7a3715da82aa11553043285a768c9fb
Olha Kudriashova <oliakudriashova15@gmail.com>
2020-08-03 15:22:36 +0300

    Remove unused propTypes

--- src/components/TodoBoard/TodoBoard.js
+++ src/components/TodoBoard/TodoBoard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import TodoList from '../TodoList/TodoList';
 import { OpenModalButton } from './StyledComponents';
 import AddingTodoPanel from '../AddingTodoPanel/AddingTodoPanel';
@@ -45,6 +44,7 @@ const TodoBoard = () => {
           isModalOpened={isModalOpened}
           setModalOpen={setIsModalOpened}
           inputText={inputText}
+          setIsModalOpened={setIsModalOpened}
         />
       ) : null}
       <TodoList
@@ -58,33 +58,5 @@ const TodoBoard = () => {
   );
 };
 
-TodoBoard.propTypes = {
-  setInputText: PropTypes.func.isRequired,
-  addTodo: PropTypes.func.isRequired,
-  toggleTodo: PropTypes.func.isRequired,
-  searchTodos: PropTypes.func.isRequired,
-  deleteTodo: PropTypes.func.isRequired,
-  setSortType: PropTypes.func.isRequired,
-  setDate: PropTypes.func.isRequired,
-  setFilterDate: PropTypes.func.isRequired,
-  setModalOpen: PropTypes.func.isRequired,
-  isModalOpened: PropTypes.bool.isRequired,
-  inputText: PropTypes.string.isRequired,
-  todos: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired,
-      completed: PropTypes.bool.isRequired,
-    }),
-  ).isRequired,
-  filters: PropTypes.objectOf({
-    text: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-  }).isRequired,
-  sortType: PropTypes.string.isRequired,
-  sortOptions: PropTypes.arrayOf(
-    PropTypes.shape({ type: PropTypes.string.isRequired, value: PropTypes.string.isRequired }),
-  ).isRequired,
-};
 
 export default TodoBoard;
