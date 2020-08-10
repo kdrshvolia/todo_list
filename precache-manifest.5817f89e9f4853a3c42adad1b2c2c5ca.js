fa838ab045990a9bedc9b0603d5b05bbd4235eaa
Olha Kudriashova <oliakudriashova15@gmail.com>
2020-08-10 19:44:02 +0300

    Updates

---
+++ precache-manifest.5817f89e9f4853a3c42adad1b2c2c5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86412c4905eeae73142dcdf6fac3190d",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "eed0b49a53cf838ce32f",
+    "url": "/todo_list/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "d59d9cb28c825d799d13",
+    "url": "/todo_list/static/js/2.583a4aa4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_list/static/js/2.583a4aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed0b49a53cf838ce32f",
+    "url": "/todo_list/static/js/main.1e526051.chunk.js"
+  },
+  {
+    "revision": "cf74516e357f9a0d23f8",
+    "url": "/todo_list/static/js/runtime-main.7a3e04cd.js"
+  }
+]);
